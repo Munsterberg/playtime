43f099a01abd1d38f9b0397c0b2f39bc54e2dcd8
Munsterberg <jetrussell93@gmail.com>
2017-03-12 21:37:52 -0400

    install react-router-dom and config router

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import '../../public/normalize.css';
 import '../../public/style.css';
+
+import Landing from './Landing';
 
 class App extends React.Component {
     render() {
         return (
-            <div>test</div>
+            <HashRouter>
+                <div className="app">
+                    <Route exactly pattern="/" component={Landing} />
+                </div>
+            </HashRouter>
         );
     }
 }
