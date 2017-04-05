0ddd95a8e73ad6cd479daf616f9517c84765d740
Munsterberg <jetrussell93@gmail.com>
2017-04-04 22:12:16 -0400

    add search input test

---
+++ src/js/Search/__tests__/index.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Search from '../index';
+import ShowCard from '../../ShowCard';
+import preload from '../../../../public/data.json';
+
+describe('<Search />', () => {
+    let wrapper = null;
+
+    beforeEach(() => {
+        wrapper = shallow(<Search />);
+    });
+
+    it('should render correct component', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render correct number', () => {
+        expect(preload.shows.length).toEqual(wrapper.find(ShowCard).length);
+    });
+
+    it('should render correct amount of shows', () => {
+        const searchTerm = 'house';
+        const searchInput = wrapper.find('input');
+        searchInput.simulate('change', {target: {value: searchTerm}});
+        expect(wrapper.find(ShowCard).length).toEqual(2);
+    });
+});
